09e66f78883d08efa2f90768f2110e00876c4671
Nick Hoyle <nick@pushmyprofile.co.uk>
2014-11-28 17:18:02 +0000

    Amended Footer
    Changes to footer, widget areas and css styles

--- footer.php
+++ footer.php
@@ -1,8 +1,27 @@
 </section>
-<footer class="row">
+<div class="sub-footer">
+  <div class="row">  
+      <div class="small-12 large-8 columns">
+<h2>Have you Questions? Need More Information?</h2>
+      </div>
+      <div class="small-12 large-4 columns">
+<a class="button large round" href="contact-us/">Get In Touch</a>
+      </div>
+</div>
+    </div>
+<footer id="site-footer">
+    <div class="row">
 	<?php do_action('foundationPress_before_footer'); ?>
 	<?php dynamic_sidebar("footer-widgets"); ?>
+        </div>
+    <div class="row">
+        <hr />
+        <div class="large-12 columns">
+        <p>2014 &copy; Malibu Hot Tubs</p>
+        </div>
+        
 	<?php do_action('foundationPress_after_footer'); ?>
+    </div>
 </footer>
 <a class="exit-off-canvas"></a>
 
